2a39704cdff9f4540e91b29bfb5aad97280255a3
Jessicajeje <jessicasteffany598@gmail.com>
2026-02-26 16:51:57 -0300

    implementação do spring boot, criação de container no docker e teste de API

---
+++ src/main/java/br/com/ifpe/oxefood_api_jessica/OxefoodApiJessicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood_api_jessica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiJessicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiJessicaApplication.class, args);
+	}
+
+}
